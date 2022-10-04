0c876abcf8968d9025c736eb4bb86fea42adcd13
iljoochae <ddepapa@iljoochaes-MBP.fios-router.home>
2022-10-03 23:19:16 -0400

    created database and auto time setting

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
+    #auto_now_add = True (추가 되었을 시 자동으로 시간 추가)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author: 추후 작성
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
