175acf7997d00018b1a8b8813fe1bbfaf9757580
iljoochae <ddepapa@iljoochaes-MBP.fios-router.home>
2022-10-05 22:56:12 -0400

    by CBV, created blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
@@ -19,15 +19,19 @@ class PostList(ListView):
 #     )
 
 
+class PostDetail(DetailView):
+    model = Post
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': post,
-        }
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/single_page.html',
+#         {
+#             'post': post,
+#         }
+#
+#     )
 
-    )
+
